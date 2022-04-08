0764817420854b76b941b0b21ba5116263d71528
korolvd <ffirin@mail.ru>
2022-04-09 00:10:53 +0500

    2. IndexControl. Таблица и вид. [#2092]

--- src/main/java/ru/job4j/accident/control/IndexController.java
+++ src/main/java/ru/job4j/accident/control/IndexController.java
@@ -3,20 +3,20 @@ package ru.job4j.accident.control;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.ArrayList;
-import java.util.List;
+import ru.job4j.accident.service.AccidentService;
 
 @Controller
 public class IndexController {
 
+    private final AccidentService service;
+
+    public IndexController(AccidentService service) {
+        this.service = service;
+    }
+
     @GetMapping("/accident")
     public String index(Model model) {
-        List<String> list = new ArrayList<>();
-        for (int i = 1; i <= 5; i++) {
-            list.add("String " + i);
-        }
-        model.addAttribute("list", list);
+        model.addAttribute("accidents", service.findAll());
         return "index";
     }
 }
---
+++ src/main/java/ru/job4j/accident/service/AccidentService.java
@@ -0,0 +1,20 @@
+package ru.job4j.accident.service;
+
+import org.springframework.stereotype.Service;
+import ru.job4j.accident.model.Accident;
+import ru.job4j.accident.store.AccidentMemStore;
+
+import java.util.Collection;
+
+@Service
+public class AccidentService {
+    private final AccidentMemStore store;
+
+    public AccidentService(AccidentMemStore store) {
+        this.store = store;
+    }
+
+    public Collection<Accident> findAll() {
+        return store.findAll();
+    }
+}
