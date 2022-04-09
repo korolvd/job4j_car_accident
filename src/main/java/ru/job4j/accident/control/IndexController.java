59ea2b4c1f72d6eb505df3847c2b95e62e04d930
korolvd <ffirin@mail.ru>
2022-04-09 09:53:51 +0500

    4. Form с композиционным объектом [#305522]

--- src/main/java/ru/job4j/accident/control/IndexController.java
+++ src/main/java/ru/job4j/accident/control/IndexController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.job4j.accident.model.Accident;
+import ru.job4j.accident.model.AccidentType;
 import ru.job4j.accident.service.AccidentService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -26,6 +30,11 @@ public class IndexController {
 
     @GetMapping("/addForm")
     public String addAccident(Model model) {
+        List<AccidentType> types = new ArrayList<>();
+        types.add(AccidentType.of(1, "Две машины"));
+        types.add(AccidentType.of(2, "Машина и человек"));
+        types.add(AccidentType.of(3, "Машина и велосипед"));
+        model.addAttribute("types", types);
         return "addForm";
     }
 
---
+++ src/main/java/ru/job4j/accident/model/AccidentType.java
@@ -0,0 +1,29 @@
+package ru.job4j.accident.model;
+
+public class AccidentType {
+    private int id;
+    private String name;
+
+    public static AccidentType of(int id, String name) {
+        AccidentType type = new AccidentType();
+        type.id = id;
+        type.name = name;
+        return type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
