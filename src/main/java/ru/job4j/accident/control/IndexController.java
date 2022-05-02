51a581f6a16cb66dfdabd4d9e8fd0aefed532761
korolvd <ffirin@mail.ru>
2022-05-02 14:45:59 +0500

    1. Авторизация через JDBC [#2094]

--- src/main/java/ru/job4j/accident/control/IndexController.java
+++ src/main/java/ru/job4j/accident/control/IndexController.java
@@ -1,5 +1,6 @@
 package ru.job4j.accident.control;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,6 +34,7 @@ public class IndexController {
 
     @GetMapping("/index")
     public String index(Model model) {
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         model.addAttribute("accidents", accidentService.findAll());
         return "index";
     }
